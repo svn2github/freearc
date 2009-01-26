eba91d2170f62e8ea8df5ea3002452779f89a413
j-o <j-o@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-01-26 22:01:17 +0000

    Tornado/build.sh: fix -flags option, respect CC and CFLAGS environment variables
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@10 3a4f7f31-9599-433d-91b1-573e8b61252c

--- Compression/Tornado/build.sh
+++ Compression/Tornado/build.sh
@@ -2,12 +2,14 @@
 
 # Tornado build script for Unix, written by Joachim Henke
 
-PARM='-D_FILE_OFFSET_BITS=64 -DFREEARC_UNIX -lm -otor main.cpp'
+CC=${CC:-gcc}
+CFLAGS=${CFLAGS:--O3 --param inline-unit-growth=999 -fomit-frame-pointer -fstrict-aliasing -ffast-math -fforce-addr -fno-exceptions -fno-rtti -fno-threadsafe-statics -fwhole-program -combine}
+
+PARM=''
 PARM_ADDR='-m32'
 PARM_DEBUG=''
 PARM_ENDIANNES='-DFREEARC_INTEL_BYTE_ORDER'
 PARM_FULL=''
-PARM_OPT='-O3 --param inline-unit-growth=999 -fomit-frame-pointer -fstrict-aliasing -ffast-math -fforce-addr -fno-exceptions -fno-rtti -fno-threadsafe-statics -fwhole-program -combine'
 PARM_PROF='-funroll-loops'
 PARM_STATS=''
 PARM_TIME='-lrt'
@@ -27,7 +29,7 @@ for P; do
 			;;
 		'-debug')
 			PARM_DEBUG='-DDEBUG -g'
-			PARM_OPT=''
+			CFLAGS=''
 			;;
 		"-flags="*)
 			PARM="${P#*=} "
@@ -46,14 +48,15 @@ for P; do
 			;;
 		'-h')
 			echo 'usage: build.sh [OPTIONS] [FILE_FOR_PROFILING]'
-			echo 'possible options: -full -profiled -64 -be -notiming -stats -debug -flags="<gcc flags>"'
+			echo 'possible options:'
 			echo '  -full      -  generate code for all possible coder/matchfinder combinations'
 			echo '  -profiled  -  optimize program using FILE_FOR_PROFILING as sample data'
 			echo '  -64        -  compile 64-bit version'
 			echo '  -be        -  compile for big-endian CPUs (PowerPC, Sparc, Motorola...)'
 			echo '  -notiming  -  remove speed calculation code'
-			echo '  -stats     -  print program''s internal compression ststistics'
+			echo '  -stats     -  print program'\''s internal compression ststistics'
 			echo '  -debug     -  add debugging info and printfs'
+			echo '  -flags=... -  use additional compiler flags'
 			exit 0
 			;;
 		*)
@@ -62,17 +65,17 @@ for P; do
 		esac
 	fi
 done
-PARM="$PARM $PARM_ENDIANNES $PARM_ADDR $PARM_TIME $PARM_OPT"
+PARM="$PARM$PARM_ENDIANNES $PARM_ADDR $PARM_TIME $CFLAGS -D_FILE_OFFSET_BITS=64 -DFREEARC_UNIX -lm -otor main.cpp"
 
 if [ "$PARM_PROF" = '-fprofile-use' ]; then
 	echo 'compiling binary for profiling...'
-	gcc $PARM -fprofile-generate || exit 1
+	$CC $PARM -fprofile-generate || exit 1
 	for M in `seq 1 11`; do
 		echo "profiling method $M..."
 		./tor -$M -q <$TEST_FILE >temp.tor && \
-		./tor -d -q <temp.tor >/dev/null || break
+		./tor -d -q <temp.tor >/dev/null || exit 1
 	done
 	rm -f temp.tor
 fi
 echo 'compiling final binary...'
-gcc $PARM $PARM_PROF -s
+$CC $PARM $PARM_PROF -s
