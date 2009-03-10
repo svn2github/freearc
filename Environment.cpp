e3e28d5533ab3de82034d910e18cd173f5950a8e
bulatz <bulatz@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-03-10 17:26:20 +0000

    GUI: fixed Run command, +1
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@143 3a4f7f31-9599-433d-91b1-573e8b61252c

--- Environment.cpp
+++ Environment.cpp
@@ -214,7 +214,6 @@ void RunFile (const CFILENAME filename, const CFILENAME curdir, int wait_finish)
   sei.cbSize = sizeof(SHELLEXECUTEINFO);
   sei.fMask = (wait_finish? SEE_MASK_NOCLOSEPROCESS : 0);
   sei.hwnd = GetActiveWindow();
-//  sei.lpVerb = _T("open");
   sei.lpFile = filename;
   sei.lpDirectory = curdir;
   sei.nShow = SW_SHOW;
