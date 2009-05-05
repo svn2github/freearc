e79dd5ae267c8e6eef2a9b3a4bcf6b0f257c1e54
bulatz <bulatz@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-05-05 17:57:09 +0000

    ArcShellExt: its own GUID
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@317 3a4f7f31-9599-433d-91b1-573e8b61252c

--- ArcShellExt/ArcShellExt.h
+++ ArcShellExt/ArcShellExt.h
@@ -1,22 +1,17 @@
 //---------------------------------------------------------------------------
 // Copyright 2002-2008 Andre Burgaud <andre@burgaud.com>
 // See license.txt
-// $Id: wscitecm.h 497 2008-12-07 03:17:37Z andre $
+// $Id: ArcShellExt.h 497 2008-12-07 03:17:37Z andre $
 //---------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------
-// wscitecm.h
+// ArcShellExt.h
 //---------------------------------------------------------------------------
 
-// Old GUID
-// {120b94b5-2e6a-4f13-94d0-414bcb64fa0f}
-//DEFINE_GUID(CLSID_ShellExtension, 0x120b94b5, 0x2e6a, 0x4f13, 0x94, 0xd0, 0x41, 0x4b, 0xcb, 0x64, 0xfa, 0x0f);
-
-// New GUID since version 1.3.1
-// Created to avoid conflict with Notpad++ reusing this code with the GUID above
-// {C4ACBD3E-6114-4618-904C-B206ABA9DEB0}
+// GUID
+// {1539B32D-3A75-4a4d-9B38-5A6000EA7045}
 DEFINE_GUID(CLSID_ShellExtension,
-            0xc4acbd3e, 0x6114, 0x4618, 0x90, 0x4c, 0xb2, 0x6, 0xab, 0xa9, 0xde, 0xb0);
+            0x1539b32d, 0x3a75, 0x4a4d, 0x9b, 0x38, 0x5a, 0x60, 0x0, 0xea, 0x70, 0x45);
 
 class CShellExtClassFactory : public IClassFactory {
 protected:
