6fd10d772f953153bd0c7d7d3c6692fdc9610c5b
bulatz <bulatz@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-06-04 07:12:15 +0000

    CLS: comments, __cdecl, pack(1), CLS_FLUSH
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@375 3a4f7f31-9599-433d-91b1-573e8b61252c

--- Compression/_CLS/cls.h
+++ Compression/_CLS/cls.h
@@ -1,23 +1,38 @@
 // CLS version 0.10
 #define CLS_API_VERSION 10
 
-// Type of callback passed to ClsMain
-typedef int CLS_CALLBACK(void* instance, int op, void *ptr, int n);
+#include <stdlib.h>
+#include <string.h>
 
-// Type of ClsMain
-typedef int CLS_MAIN(int op, CLS_CALLBACK callback, void* instance);
+// All CLS structures are packed in order to avoid compiler incompatibilities
+#pragma pack(push, CLS, 1)
+
+// Type of callback passed to ClsMain (see below)
+//   instance           - identifies this particular ClsMain call
+//   callback_operation - callback operation requested (CLS_FULL_READ, CLS_GET_PARAMSTR and so on)
+//   ptr, n             - generic parameters, whose interpretation depends on particular operation. If more parameters are required, ptr points to the record
+//   returns negative error code on failure and for some operations informative positive value
+typedef int __cdecl CLS_CALLBACK (void* instance, int callback_operation, void *ptr, int n);
+
+// Type of ClsMain. It's the one and only function that implements all the codec functionality
+//   operation - operation requested (CLS_COMPRESS and so on)
+//   callback  - function that compressor calls to receive any functionality from the caller (including I/O and compression parameters)
+//   instance  - passed to callback to distinguish this particular call
+//   returns negative error code on failure and for some operations informative positive value
+typedef int __cdecl CLS_MAIN (int operation, CLS_CALLBACK callback, void* instance);
 
 // Function that implements whole codec functionality
 extern "C" CLS_MAIN ClsMain;
 
 // Operations
-const int CLS_INIT                          = 1;       // Called once on DLL load
-const int CLS_DONE                          = 2;       // Called once before DLL unload
-const int CLS_COMPRESS                      = 3;       // Request to compress data using I/O via callbacks
-const int CLS_DECOMPRESS                    = 4;       // Request to decompress data using I/O via callbacks
+const int CLS_INIT                          = 1;       // Called on codec load in order to get more info about it
+const int CLS_DONE                          = 2;       // Called before code unload
+const int CLS_FLUSH                         = 6;       // Called after batch of operations were performed in order to allow codec to release resources
+const int CLS_COMPRESS                      = 3;       // Request to compress (encode) data using I/O via callbacks
+const int CLS_DECOMPRESS                    = 4;       // Request to decompress (decode) data using I/O via callbacks
 const int CLS_PREPARE_METHOD                = 5;       // Apply all restrictions to method and then return its new formatted params and info (memreqs/blocksize/threads...)
 
-// Callbacks
+// Callback operations
 const int CLS_FULL_READ                     = 4096;    // Read data into buffer ptr:n. Use CLS_FULL_READ+i to read i'th stream. Retcode: <0 - error, 0 - EOF, >0 - amount of data read
 const int CLS_PARTIAL_READ                  = 5120;    // Read data partially (buffer may be not filled entirely even if EOF isn't reached). CLS_PARTIAL_READ+i also works. Retcode: the same
 const int CLS_FULL_WRITE                    = 6144;    // Write data from buffer ptr:n. CLS_FULL_WRITE+i also works. Retcode: the same
@@ -31,8 +46,8 @@ const int CLS_MEMORY                        = 6;       // How much memory?
 const int CLS_DECOMPRESSION_MEMORY          = 7;       // How much memory for decompression?
 const int CLS_DECOMPRESSOR_VERSION          = 8;       // Codec version required for decompression
 const int CLS_BLOCK                         = 9;       // Block size (for block-wise compressors like bwt)
-const int CLS_EXPAND_DATA                   = 10;      // This compressor may expand data (like precomp)?
-// INIT-stage callbacks
+const int CLS_EXPAND_DATA                   = 10;      // May this compressor expand data (like precomp)?
+// INIT-stage callback operations
 const int CLS_ID                            = 101;     // Codec ID as unique ASCIIZ string (for example, "lzma.7zip.org")
 const int CLS_VERSION                       = 102;     // Codec version as ASCIIZ string (for example, "1.2.3")
 const int CLS_THREAD_SAFE                   = 103;     // Is the DLL thread-safe, i.e. allows to run multiple copies of ClsMain() simultaneously?
@@ -59,7 +74,7 @@ const int CLS_PARAM_MEMORY_MB               = -3;      // Memory  parameter, lik
 const int CLS_MAX_PARAMSTR_SIZE             = 256;     // Maximum size of string returned by CLS_GET_PARAMSTR
 const int CLS_MAX_ERROR_MSG                 = 256;     // Maximum size of error message
 
-// Macro that simplifies verifying operations' success
+// Macro that simplifies verifying operation success
 #define CHECK_CLS_OK(action)  {int cls_retcode = (action);  if (cls_retcode!=CLS_OK)  return cls_retcode;}
 
 /* to do:
@@ -67,13 +82,13 @@ const int CLS_MAX_ERROR_MSG                 = 256;     // Maximum size of error 
 threads) and whether dll is thread-safe or not (if not, it can be secured
 by loading multiple instances of dll - might be a useful feature as many
 experimental compressors are not really incapsulated).
-- codecs need to know how much memory for compression / decompression are they supposed to use.
+- codecs need to know how much memory for compression / decompression they are supposed to use.
 - some codecs might handle multithreading in a smarter way than splitting streams.
 
 - check versions and backward/forward compatibility
 - allow multiple codecs in same dll. this may be solved by ClsMain2, ClsMain3... exported
 but this may be not enough for some more complex scenarios
-- some interface methods are required for initialization and model flush
++ some interface methods are required for initialization and model flush
 (which are not the same as there might be some precalculation required
 only once).
 - what about detectors
@@ -111,7 +126,7 @@ class ClsCompressor
 {
 protected:
     // Saved parameters passed to ClsMain
-    int op; CLS_CALLBACK callback; void* instance;
+    int operation; CLS_CALLBACK callback; void* instance;
 
     // Descendant-overridden functions
     virtual void load()       {throw ClsException(CLS_ERROR_NOT_IMPLEMENTED);}  // Called just once on DLL load
@@ -147,18 +162,18 @@ protected:
 
 
     // Perform checked callback and raise exception if operation fails
-    virtual int checked_callback(int callbackop, void *ptr, int n)
+    virtual int checked_callback(int callback_operation, void *ptr, int n)
     {
-        int result = callback(instance, callbackop, ptr, n);
+        int result = callback(instance, callback_operation, ptr, n);
         if (result < 0)
             throw ClsException(result);
         return result;
     }
 
     // Perform checked callback and raise exception if operation fails (except for case it's not implemented)
-    virtual int optional_callback(int callbackop, void *ptr, int n)
+    virtual int optional_callback(int callback_operation, void *ptr, int n)
     {
-        int result = callback(instance, callbackop, ptr, n);
+        int result = callback(instance, callback_operation, ptr, n);
         if (result < 0  &&  result != CLS_ERROR_NOT_IMPLEMENTED)
             throw ClsException(result);
         return result;
@@ -170,7 +185,7 @@ public:
     {
         try
         {
-            switch(op)
+            switch(operation)
             {
             case CLS_INIT:       load(); break;
             case CLS_DONE:       unload(); break;
@@ -183,7 +198,7 @@ public:
                     optional_callback(CLS_GET_PARAMSTR, params, CLS_MAX_PARAMSTR_SIZE);
                     parse_params(params);
                     apply_constraints();
-                    switch(op)
+                    switch(operation)
                     {
                     case CLS_COMPRESS:        compress(); break;
                     case CLS_DECOMPRESS:      decompress(); break;
@@ -219,10 +234,14 @@ optionally:
     ...
 };
 
-extern "C" int ClsMain(int op, CLS_CALLBACK callback, void* instance)
+extern "C" int __cdecl ClsMain(int operation, CLS_CALLBACK callback, void* instance)
 {
-    return MyCompressor(op, callback, instance).run();
+    return MyCompressor(operation, callback, instance).run();
 }
 */
 
 #endif
+
+
+#pragma pack(pop, CLS)
+
