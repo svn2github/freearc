929701186889666df9ffb14d7fb2dac915b1537f
bulatz <bulatz@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-07-28 12:26:15 +0000

    Installer: moves ini\* files to bin directory for portable version
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@464 3a4f7f31-9599-433d-91b1-573e8b61252c

--- Installer/release3.cmd
+++ Installer/release3.cmd
@@ -2,13 +2,17 @@ set VER=0.52
 ::set V=-alpha-2009-04-19
 
 del FreeArc*.exe FreeArc*.zip FreeArc*.bz2 FreeArc*.arc
-arc a FreeArc-console FreeArc.url Addons\ -xFreeArc.reg bin\arc.* bin\facompress.dll bin\*.sfx bin\unarc.* bin\empty.arc bin\scripts\ Documentation\ -xFreeArc-GUI*.* -xDocumentation\readme.txt -z=Documentation\readme.txt -m5x -mm- -sfx=bin\freearc-tiny.sfx
 
 "C:\Program Files (x86)\NSIS\makensis.exe"       FreeArc.nsi
 "C:\Program Files (x86)\NSIS\makensis.exe" -DGTK FreeArc.nsi
 
 ren FreeArc-install.exe FreeArc-%VER%-win32%V%.exe
 ren FreeArc-update.exe  FreeArc-update-%VER%-win32%V%.exe
+
+move ini\* bin
+rmdir ini
+
+arc a FreeArc-console FreeArc.url Addons\ -xFreeArc.reg bin\arc.* bin\facompress.dll bin\*.sfx bin\unarc.* bin\empty.arc bin\scripts\ Documentation\ -xFreeArc-GUI*.* -xDocumentation\readme.txt -z=Documentation\readme.txt -m5x -mm- -sfx=bin\freearc-tiny.sfx
 ren FreeArc-console.exe FreeArc-console-%VER%-win32%V%.exe
 
 7z -mx a FreeArc-portable-%VER%-win32%V%.zip  -x!GTK* -x!FreeArc-* -x!*.nsi -x!*.nsh -x!*.cmd
