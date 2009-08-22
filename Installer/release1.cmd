4edc99bd8d2a06a047e6a82c18d807a30e5cb04e
bulatz <bulatz@3a4f7f31-9599-433d-91b1-573e8b61252c>
2009-08-22 08:13:46 +0000

    Installer: added ArcShellExt, all2arc.exe, 7zg, inno setup dll & script
    git-svn-id: https://freearc.svn.sourceforge.net/svnroot/freearc@526 3a4f7f31-9599-433d-91b1-573e8b61252c

--- Installer/release1.cmd
+++ Installer/release1.cmd
@@ -1,12 +1,20 @@
+::Compile all components
 cd ..\Unarc
 make
 ::upx -d *.sfx
 ::upx --ultra-brute *.sfx
 upx --lzma -9 *.sfx
 cd ..
+
 call compile-O2.cmd
 call compile-GUI-O2.cmd
+
 cd Compression
 del *.obj facompress.dll
 call compile-dll.cmd
+
+cd ..\ArcShellExt
+call compile.cmd
+call compile-64.cmd
+
 cd ..\Installer
--- Installer/release2.cmd
+++ Installer/release2.cmd
@@ -5,14 +5,17 @@ cd ..
 del !Release /s/q
 mkdir !Release
 mkdir !Release\Addons
+mkdir !Release\Addons\InnoSetup
 mkdir !Release\bin
 mkdir !Release\bin\arc.languages
+mkdir !Release\bin\ArcShellExt
 mkdir !Release\Documentation
 mkdir !Release\GTK2-Runtime
 mkdir !Release\gtk2-themes
 mkdir !Release\ini
 
 xcopy Addons        !Release\Addons        /e
+xcopy 7-zip         !Release\bin           /e
 xcopy bin           !Release\bin           /e
 xcopy ini           !Release\ini           /e
 xcopy Documentation !Release\Documentation /e
@@ -20,20 +23,27 @@ xcopy GTK2-Runtime  !Release\GTK2-Runtime  /e
 xcopy gtk2-themes   !Release\gtk2-themes   /e
 xcopy arc.languages\arc.languages  !Release\bin\arc.languages        /e
 
-xcopy ..\Documentation\History.txt          !Release\Addons
-xcopy ..\Unarc\FreeArc.fmt                 "!Release\Addons\FAR MultiArc plugin"
-xcopy ..\Documentation\FreeArc036-eng.htm   !Release\Documentation
-xcopy ..\Documentation\FreeArc040-rus.htm   !Release\Documentation
-xcopy ..\Documentation\FreeArc-GUI-Eng.htm  !Release\Documentation
-xcopy ..\Documentation\FreeArc-GUI-Rus.htm  !Release\Documentation
-xcopy ..\Tests\Arc.exe                      !Release\bin
-xcopy ..\Tests\FreeArc.exe                  !Release\bin
-xcopy ..\Unarc\unarc.exe                    !Release\bin
-xcopy ..\Unarc\*.sfx                        !Release\bin
-xcopy ..\Compression\facompress.dll         !Release\bin
-xcopy release3.cmd                          !Release
-xcopy *.nsi                                 !Release
-xcopy *.nsh                                 !Release
-xcopy FreeArc.url                           !Release
+xcopy ..\Documentation\History.txt                      !Release\Addons
+xcopy ..\Unarc\FreeArc.fmt                             "!Release\Addons\FAR MultiArc plugin"
+xcopy ..\Unarc\unarc.dll                                !Release\InnoSetup
+xcopy ..\Unarc\InnoSetup\freearc_example.iss            !Release\InnoSetup
+xcopy ..\Unarc\InnoSetup\FreeArc_Example-Ext.iss        !Release\InnoSetup
+xcopy ..\Unarc\InnoSetup\readme.txt                     !Release\InnoSetup
+xcopy ..\Documentation\FreeArc036-eng.htm               !Release\Documentation
+xcopy ..\Documentation\FreeArc040-rus.htm               !Release\Documentation
+xcopy ..\Documentation\FreeArc-GUI-Eng.htm              !Release\Documentation
+xcopy ..\Documentation\FreeArc-GUI-Rus.htm              !Release\Documentation
+xcopy ..\Tests\Arc.exe                                  !Release\bin
+xcopy ..\Tests\FreeArc.exe                              !Release\bin
+xcopy ..\Unarc\unarc.exe                                !Release\bin
+xcopy ..\Unarc\*.sfx                                    !Release\bin
+xcopy ..\Compression\facompress.dll                     !Release\bin
+xcopy ..\ArcShellExt\all2arc\all2arc.exe                !Release\bin
+xcopy ..\ArcShellExt\*.dll                              !Release\bin\ArcShellExt
+xcopy ..\ArcShellExt\*.lua                              !Release\bin\ArcShellExt
+xcopy release3.cmd                                      !Release
+xcopy *.nsi                                             !Release
+xcopy *.nsh                                             !Release
+xcopy FreeArc.url                                       !Release
 
 cd    !Release
